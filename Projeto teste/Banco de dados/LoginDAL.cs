c6a06cb63fbb8930860d17b59aa52540946b8cc1
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2025-04-25 18:47:34 -0300

    feat(auth): adiciona criptografia de senha usando SHA256 no login do usuário fix(auth): adiciona tratamento de exceções para erros no LoginDAL e falhas gerais

--- Projeto teste/Banco de dados/LoginDAL.cs
+++ Projeto teste/Banco de dados/LoginDAL.cs
@@ -9,14 +9,31 @@ using Regras_de_negócio;
 namespace Data_Access {
     public class LoginDAL {
         public bool VerificarLogin(string Email, string Senha) {
-            string query = "SELECT COUNT(*) FROM dados_pessoais WHERE Email = @Email AND Senha = @Senha";
-            using (var conexao = ConexaoDAL.Abrir())
-            using (MySqlCommand comando = new MySqlCommand(query, conexao)) {
-                comando.Parameters.AddWithValue("@Email", Email);
-                comando.Parameters.AddWithValue("@Senha", Senha);
-                int count = Convert.ToInt32(comando.ExecuteScalar());
-                return count > 0;
+
+
+            if (string.IsNullOrWhiteSpace(Email))
+                throw new ArgumentException("O e-mail não pode estar vazio.");
+            if (string.IsNullOrWhiteSpace(Senha))
+                throw new ArgumentException("A senha não pode estar vazio.");
+
+            try {
+                string query = "SELECT COUNT(*) FROM dados_pessoais WHERE Email = @Email AND Senha = @Senha";
+                using (var conexao = ConexaoDAL.Abrir())
+                using (MySqlCommand comando = new MySqlCommand(query, conexao)) {
+                    comando.Parameters.AddWithValue("@Email", Email);
+
+                    string senhaHash = CriptografiaDAL.GerarHashSenha(Senha);
+                    comando.Parameters.AddWithValue("@Senha", senhaHash);
+
+                    int count = Convert.ToInt32(comando.ExecuteScalar());
+                    return count > 0;
+                }
             }
+            catch (MySqlException ex) {
+
+                throw new Exception("Erro no banco de dados: " + ex.Message);
+            }
+
         }
     }
 }
