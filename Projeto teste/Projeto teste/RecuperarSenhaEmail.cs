5f0f9004f1810862d171b30f440061a1778cfe3c
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2025-04-25 17:51:17 -0300

    feat(auth): separa fluxo de login por código com opção de acesso ou troca de senha

--- Projeto teste/Projeto teste/RecuperarSenhaEmail.cs
+++ Projeto teste/Projeto teste/RecuperarSenhaEmail.cs
@@ -16,6 +16,12 @@ namespace Projeto_teste {
     public partial class RecuperarSenhaEmail : Form {
 
         private string codigoGerado;
+        private string EmailUsuario;
+
+        public string EmailUsuarioPublico {
+            get { return EmailUsuario; }
+            set { EmailUsuario = value; }
+        }
 
         public string CodigoGerado() {
             RecuperarSenhaBLL _codigo = new RecuperarSenhaBLL();
@@ -30,17 +36,17 @@ namespace Projeto_teste {
         private void button1_Click(object sender, EventArgs e) {
 
             RecuperarSenhaBLL _retornarnomedal = new RecuperarSenhaBLL();
-            string email = textBox2.Text;
+            EmailUsuario = textBox2.Text;
             codigoGerado = CodigoGerado();
 
             EmailBLL.EnviarEmailBLL(
-               "Olá, " + _retornarnomedal.RetornoNome(email) + "\r\n\r\n" +
+               "Olá, " + _retornarnomedal.RetornoNome(EmailUsuario) + "\r\n\r\n" +
                "Recebemos uma solicitação para redefinir sua senha de acesso ao sistema AtendeTech.\r\n\r\n" +
                "Para confirmar essa solicitação, utilize o código abaixo dentro do aplicativo:\r\n\r\n" +
                "CÓDIGO DE VERIFICAÇÃO: " + codigoGerado + "\r\n\r\n" +
                "Se você não solicitou essa redefinição, por favor ignore esta mensagem ou entre em contato com nossa equipe de suporte.\r\n\r\n" +
                "Atenciosamente,\r\nEquipe AtendeTech\r\nsuporte@atendetech.com.br",
-               email,
+               EmailUsuario,
                "Código para redefinição de senha",
                button1
             );
@@ -61,15 +67,44 @@ namespace Projeto_teste {
         private void button2_Click_1(object sender, EventArgs e) {
             if (codigoGerado == textBox1.Text) {
 
-                this.Hide();
-                Exemplo exemplo = new Exemplo();
-                exemplo.ShowDialog();
-                this.Hide();
+                MessageBox.Show("Código verificado com sucesso!\n" +
+                    "Escolha o que deseja fazer");
+                button3.Visible = true;
+                button4.Visible = true;
+
+            }
+            else {
+                MessageBox.Show("Código invalido!");
             }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e) {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+
+            RecuperarSenhaEmail telaOriginal = new RecuperarSenhaEmail();
+            telaOriginal.EmailUsuarioPublico = textBox2.Text;
+
+            var telaNova = new TrocarSenha(telaOriginal);
+            this.Hide();
+            telaNova.ShowDialog();
+            this.Hide();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+          
+            this.Hide();
+            Exemplo exemplo = new Exemplo();
+            exemplo.ShowDialog();
+            this.Hide();
+        }
+
+        private void RecuperarSenhaEmail_Load(object sender, EventArgs e) {
+            button3.Visible = false;
+            button4.Visible = false;
         }
     }
      
---
+++ Projeto teste/Projeto teste/TrocarSenha.cs
@@ -0,0 +1,94 @@
+﻿using Regras_de_negócio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Projeto_teste {
+    public partial class TrocarSenha : Form {
+
+        private RecuperarSenhaEmail _referencia;
+
+        public TrocarSenha(RecuperarSenhaEmail tela) {
+            InitializeComponent();
+            _referencia = tela;
+        }
+
+        public TrocarSenha() {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e) {
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            textBox2.PasswordChar = checkBox1.Checked ? '\0' : '*';
+            textBox3.PasswordChar = checkBox1.Checked ? '\0' : '*';
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            string email = _referencia.EmailUsuarioPublico;
+
+            string SenhaAlterada = textBox3.Text;
+
+
+            RecuperarSenhaBLL _recuperarsenhabll = new RecuperarSenhaBLL();
+            RecuperarSenhaBLL _alterarsenhadal = new RecuperarSenhaBLL();
+            RecuperarSenhaBLL _retornarnomedal = new RecuperarSenhaBLL();
+
+                if (textBox2.Text == textBox3.Text) {
+                    label5.Visible = false;
+                    _alterarsenhadal.AlterarSenha(email, SenhaAlterada);
+                    MessageBox.Show("Senha Alterada com sucesso!");
+
+                    EmailBLL.EnviarEmailBLL(
+                     "Olá, " + _retornarnomedal.RetornoNome(email) + "\r\n\r\n" +
+                     "Informamos que sua senha foi alterada com sucesso.\r\n\r\n" +
+                     "Se você realizou essa alteração, pode desconsiderar esta mensagem.\r\n\r\n" +
+                     "Caso não tenha solicitado essa mudança, recomendamos que entre em contato imediatamente com nossa equipe de suporte para garantir a segurança da sua conta.\r\n\r\n" +
+                     "Atenciosamente,\r\nEquipe AtendeTech\r\nsuporte@atendetech.com.br",
+                     email,
+                     "Alteração de senha realizada com sucesso",
+                     button1
+                    );
+
+
+                    this.Hide();
+                    Exemplo exemplo = new Exemplo();
+                    exemplo.ShowDialog();
+                    this.Hide();
+                }
+                else {
+                    label5.Text = "Senha diferente";
+                    label5.ForeColor = Color.Red;
+                    label5.Visible = true;
+                }
+
+            
+        }
+    }
+}
